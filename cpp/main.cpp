3692d7092c9709dceff66742c10a4e35e7541ef9
Ali Gholamian <alisepehr88@gmail.com>
2020-12-06 23:30:18 +0330

    Puzzle class completed

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -3,5 +3,17 @@
 
 int main(int argc, char** argv)
 {
+    std::array<std::array<int, 3>, 3> in = {
+        std::array<int, 3> { 1, 2, 3 },
+        std::array<int, 3> { 5, 6, 0 },
+        std::array<int, 3> { 7, 8, 4 }
+    };
+    std::array<std::array<int, 3>, 3> fi = {
+        std::array<int, 3> { 1, 2, 3 },
+        std::array<int, 3> { 5, 8, 6 },
+        std::array<int, 3> { 0, 7, 4 }
+    };
+    Puzzle a { in, fi };
+    a.Solve_Puzzle(100);
     return 0;
 }
