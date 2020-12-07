3b35b2c13e14e25a5d1a126538c7b2ca2fbfdc11
Ali Gholamian <alisepehr88@gmail.com>
2020-12-07 21:23:28 +0330

    Updated for testing Set_New_Matrixes function

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -14,6 +14,20 @@ int main(int argc, char** argv)
         std::array<int, 3> { 8, 7, 0 }
     };
     Puzzle a { in, fi };
-    a.Solve_Puzzle(10000);
+    a.Solve_Puzzle(1000);
+    std::cout << "New puzzle" << std::endl;
+    std::array<std::array<int, 3>, 3> in2 = {
+        std::array<int, 3> { 1, 2, 3 },
+        std::array<int, 3> { 4, 5, 6 },
+        std::array<int, 3> { 7, 0, 8 }
+    };
+    std::array<std::array<int, 3>, 3> fi2 = {
+        std::array<int, 3> { 1, 2, 3 },
+        std::array<int, 3> { 4, 5, 6 },
+        std::array<int, 3> { 7, 8, 0 }
+    };
+
+    a.Set_New_Matrixes(in2, fi2);
+    a.Solve_Puzzle(1000);
     return 0;
 }
