555c5f937586509ef8c09a5a7fb6412353b89d6b
Ali Gholamian <alisepehr88@gmail.com>
2020-12-07 21:36:40 +0330

    Input of all functions changed to const

--- h/puzzle.h
+++ h/puzzle.h
@@ -12,7 +12,7 @@ class Puzzle {
 private:
     class Node {
     public:
-        Node(std::array<std::array<int, 3>, 3> mat, int level, std::shared_ptr<Node> parent);
+        Node(const std::array<std::array<int, 3>, 3>& mat, int level, const std::shared_ptr<Node>& parent);
         bool operator==(const Node& second_node) const;
         std::shared_ptr<Node> parent_of_node;
         std::array<std::array<int, 3>, 3> mat;
@@ -20,16 +20,16 @@ private:
     };
     bool is_Solvable();
     bool Check_Coordinates(int x, int y) { return (x >= 0 && x < 3 && y >= 0 && y < 3); };
-    int Calculate_Cost(std::shared_ptr<Node> input_node, int mode);
+    int Calculate_Cost(const std::shared_ptr<Node>& input_node, int mode) const;
     std::array<std::array<int, 3>, 3> initial_puzzle;
     std::array<std::array<int, 3>, 3> goal_puzzle;
     std::array<int, 4> row = { 1, 0, -1, 0 };
     std::array<int, 4> col = { 0, -1, 0, 1 };
-    void Show_Solution(std::shared_ptr<Node> all_nodes, int mode) const;
+    void Show_Solution(const std::shared_ptr<Node>& all_nodes, int mode) const;
 
 public:
-    Puzzle(std::array<std::array<int, 3>, 3> initial_puzzle, std::array<std::array<int, 3>, 3> goal_puzzle);
-    void Set_New_Matrixes(std::array<std::array<int, 3>, 3> initial_puzzle, std::array<std::array<int, 3>, 3> goal_puzzle);
+    Puzzle(const std::array<std::array<int, 3>, 3>& initial_puzzle, const std::array<std::array<int, 3>, 3>& goal_puzzle);
+    void Set_New_Matrixes(const std::array<std::array<int, 3>, 3>& initial_puzzle, const std::array<std::array<int, 3>, 3>& goal_puzzle);
     void Solve_Puzzle(int _max_depth);
 };
 
