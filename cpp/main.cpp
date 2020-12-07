56d379401d360f84972cc1d562930c3fd87d93b2
Ali Gholamian <alisepehr88@gmail.com>
2020-12-07 19:03:46 +0330

    is_Solvable Completed -> (checks if puzzle is solvable before solving)

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -4,14 +4,14 @@
 int main(int argc, char** argv)
 {
     std::array<std::array<int, 3>, 3> in = {
-        std::array<int, 3> { 0, 3, 6 },
-        std::array<int, 3> { 5, 8, 7 },
-        std::array<int, 3> { 2, 4, 1 }
+        std::array<int, 3> { 3, 1, 2 },
+        std::array<int, 3> { 4, 5, 6 },
+        std::array<int, 3> { 8, 7, 0 }
     };
     std::array<std::array<int, 3>, 3> fi = {
         std::array<int, 3> { 1, 2, 3 },
         std::array<int, 3> { 4, 5, 6 },
-        std::array<int, 3> { 7, 0, 8 }
+        std::array<int, 3> { 8, 7, 0 }
     };
     Puzzle a { in, fi };
     a.Solve_Puzzle(10000);
